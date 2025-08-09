b33d41260064aeb5bdde410fc8fb9c55e00ed1ad
Marina M <marinamolina7877@gmail.com>
2025-08-09 14:25:26 -0400

    added popup prototype

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 const cal = new CalHeatmap();
 cal.paint({});
 const container = document.getElementById("cal-heatmap")
-render(container);
+//render(container);
+
+
+
+//pop up
+const popupOverlay = document.getElementById("popupOverlay");
+const addButton = document.getElementById("add");
+
+
+addButton.addEventListener("click", function(){
+  popupOverlay.style.display = "block";
+
+});
+
+popupOverlay.addEventListener("click", function(e){
+  if (e.target === popupOverlay) {
+        popupOverlay.style.display = 'none';
+        console.log("out")
+      }
+  else {
+    console.log("in");
+  }
+
+
+});
+
+
+
